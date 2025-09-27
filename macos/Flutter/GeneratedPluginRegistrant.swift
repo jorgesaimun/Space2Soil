3a1c1173b144b46bb74e59ecfdf2efa542ae060a
jorgesaimun <saifullahsaimun1505@gmail.com>
2025-09-28 00:33:52 +0600

    feat: integrate webview_flutter package and update dependencies
    - Added webview_flutter as a direct dependency in pubspec.yaml.
    - Updated pubspec.lock to include new versions for webview_flutter and related packages.
    - Registered WebViewFlutterPlugin in GeneratedPluginRegistrant.swift for macOS support.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import file_selector_macos
 import geolocator_apple
 import path_provider_foundation
 import shared_preferences_foundation
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
@@ -17,4 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
